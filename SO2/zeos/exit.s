68b01012c4cd9632fe3a983ea76f97b590f907aa
Daniel Martinez Torrente <168561334+danimtorrente@users.noreply.github.com>
2024-09-26 22:58:15 +0200

    Add files via upload

---
+++ SO2/zeos/exit.s
@@ -0,0 +1,14 @@
+# 0 "exit.S"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 0 "<command-line>" 2
+# 1 "exit.S"
+
+
+
+
+# 1 "include/asm.h" 1
+# 6 "exit.S" 2
+# 1 "include/segment.h" 1
+# 7 "exit.S" 2
---
+++ SO2/zeos/suma.s
@@ -0,0 +1,17 @@
+# 0 "suma.S"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 0 "<command-line>" 2
+# 1 "suma.S"
+# 1 "include/asm.h" 1
+# 2 "suma.S" 2
+
+.globl addAsm; .type addAsm, @function; .align 0; addAsm:
+addAsm: pushl %ebp
+ movl %esp, %ebp
+ movl 8(%ebp), %eax
+ addl 12(%ebp), %eax
+ movl %ebp, %esp
+ popl %ebp
+ ret
